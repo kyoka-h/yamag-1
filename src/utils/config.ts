c13be278b46813df1152f9040a086c86783f1036
Kyoka <kyoka.hazumi@gmail.com>
2023-11-07 16:02:51 +0900

    For 404

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,8 +7,8 @@ require('dotenv').config({ path: `./${envFile}` })
 const parseIntWithDefalult = (v:any, def:number):number => Number.isFinite(parseInt(v)) ? parseInt(v) : def;
 
 const today = new Date()
-const recordTimeHour = parseIntWithDefalult(process.env.RECORD_HOUR, 3)
-const recordTimeMinute = parseIntWithDefalult(process.env.RECORD_MINUTE, 34)
+const recordTimeHour = parseIntWithDefalult(process.env.RECORD_HOUR, 16)
+const recordTimeMinute = parseIntWithDefalult(process.env.RECORD_MINUTE, 4)
 export const recordTime = (() => {
   // 23:59など集計が日付を挟む場合には1日前を参照する
   let target = new Date(today.getFullYear(), today.getMonth(), today.getDate(), recordTimeHour, recordTimeMinute, 0, 0)
@@ -18,11 +18,11 @@ export const recordTime = (() => {
   return target
 })()
 
-export const postTitle = process.env?.POST_TITLE || `Today's 334 Top 10`
-export const remindPostText = process.env?.REMINED_POST_TEXT || `334観測中`
-export const userName = process.env?.USER_NAME || "334_t"
+export const postTitle = process.env?.POST_TITLE || `Today's おひる404 Top 10`
+export const remindPostText = process.env?.REMINED_POST_TEXT || `おひる404観測中`
+export const userName = process.env?.USER_NAME || "404ranker"
 
-export const matcher = process.env.MATCHER || /(33-?4|:hanshin:)/
+export const matcher = process.env.MATCHER || /(404)/
 const DATABASE_URL = process.env.DATABASE_URL
 
 export const mention = {
@@ -32,8 +32,8 @@ export const mention = {
 }
 
 export const server:Server = {
-  origin: process.env?.SERVER_ORIGIN || "https://misskey.io",
-  credential: process.env.SERVER_TOKEN || ''
+  origin: process.env?.SERVER_ORIGIN || "https://misskey.secinet.jp/",
+  credential: process.env.SERVER_TOKEN || '2RhTz83hWijM7OzMDjrj0EP76x5Dn07j'
 }
 
 export const Experimental = {
